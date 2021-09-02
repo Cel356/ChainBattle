905b6fa7d3ea55f4cd38cd541d47915097984286
Carlos Lopez <eliezerlopez356@gmail.com>
2021-09-02 11:59:59 -0600

    Se agrego el Index al proyecto, solo tiene codigo generico

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	require 'templates/Pdo_Coneccion.php';
+	require 'class/manageBD.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chain Battle Cel356</title>
+</head>
+<body>
+	<div>
+		
+	</div>
+</body>
+<footer>
+	
+</footer>
+</html>
